41136320783dfe4667d216d3766c2a0d4fd54e1c
Stevi Deter <stevi.deter@gmail.com>
2017-07-29 13:46:08 -0700

    initial config

---
+++ test/first_spec.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('a feature', function() {
+    describe('a scenario', function() {
+        it('does something', function() {
+            assert(true);
+        });
+    });
+});
